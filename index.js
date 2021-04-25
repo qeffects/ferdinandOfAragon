8cb63ed81d825c6fa8b7f633bf9cd0dc58818dfc
qeffects <qfluxstudio@gmail.com>
2021-04-25 17:23:22 +0300

    more stuf

--- index.js
+++ index.js
@@ -32,11 +32,13 @@ const findRole = (gld, roleid) => {
 
 const findChannel = (gld, channelid) => {
     return gld.guild.channels.resolve(channelid);
+    
 }
 
 const loadGuild = (gld) => {
     jsonfile.readFile(gld.id+'.json').then((obj) => {
         guilds[gld.id] = obj;
+
         guilds[gld.id].guild = gld;
     })
 }
@@ -51,21 +53,9 @@ const saveGuild = (gld) => {
     }, {flag: 'w'}).catch(reason => console.log(reason));
 }
 
-client.on('ready', () => {
-    console.log('I am ready!');
-    if (client.guilds){
-        client.guilds.cache.each((gld) => loadGuild(gld));
-    }
-});
+const onMessageHandler = (message) => {
 
-client.on('guildCreate', (gld) => {
-    console.log('added to a new guild');
-    guilds[gld.id] = {guild: gld, status: 'no-channel', messages: {}};
-    jsonfile.writeFile(gld.id+'.json', {status: 'setup'});
-});
-
-client.on('message', (message) => {
-    if (!message.guild) message.reply('Neslīdi man dm-os 😉');
+    if (!message.guild) return message.author != client.user ? message.reply('Neslīdi man dm-os 😉') : null;
 
     console.log(message.content);
 
@@ -76,73 +66,150 @@ client.on('message', (message) => {
             message.channel.send(messages.setupHelp);
         } else if (message.content.startsWith('%inqChannel')){
             console.log('added inquisition channel');
+
             gld.inquisition_channel = message.mentions.channels.first(1)[0].id;
+
             message.channel.send(`Inkvizīcijas kanāls uzstādīts ${message.mentions.channels.first(1)[0]}`);
             if (gld.inquisition_channel && gld.inquisition_role){
                 gld.status = 'ready';
+
                 saveGuild(gld);
-                message.channel.send('Inkvizīcija gatava, sāc rakstīt jautājumus ar %ask <jautājums>, nosauc nākamo inkvizējamo ar %target @<user>, un sāc inkvizīciju ar %start (pilnās insturkcijas ar %help)');
+
+                message.channel.send('Inkvizīcija gatava, sāc rakstīt jautājumus ar %ask <jautājums>, nosauc nākamo inkvizējamo ar %target @<user>, un sāc inkvizīciju ar %start (apskati pilnās insturkcijas ar %help)');
             }
         } else if (message.content.startsWith('%inqRole')){
             console.log('added inqusition role');
+
             gld.inquisition_role = message.mentions.roles.first(1)[0].id;
+
             message.channel.send(`Inkvizīcijas role uzstādīta ${message.mentions.roles.first(1)[0]}`);
-            //console.log(guilds[message.guild.id])
             if (gld.inquisition_channel && gld.inquisition_role){
                 console.log('ready');
                 gld.status = 'ready';
+
                 saveGuild(gld);
-                message.channel.send('Inkvizīcija gatava, sāc rakstīt jautājumus ar %ask <jautājums>, nosauc nākamo inkvizējamo ar %target @<user>, un sāc inkvizīciju ar %start (pilnās insturkcijas ar %help)');
+
+                message.channel.send('Inkvizīcija gatava, sāc rakstīt jautājumus ar %ask <jautājums>, nosauc nākamo inkvizējamo ar %target @<user>, un sāc inkvizīciju ar %start (apskati pilnās instrukcijas ar %help)');
             }
         }
     }
-
     
     if ((gld.status === 'ready'||gld.status === 'in-progress') && message.member.hasPermission('ADMINISTRATOR')){
         if (message.content.startsWith('%help')){
             message.channel.send(messages.fullHelp);
         } else if (message.content.startsWith('%ask')){
             gld.messages.push(message.content.slice(5));
+            if (gld.status ==='in-progress'){
+                if ((findChannel(gld, gld.inquisition_channel)?.lastMessage?.author == client.user) &&
+                    findChannel(gld, gld.inquisition_channel)?.lastMessage?.content.startsWith('**Pagaidām jautājumu nav') &&
+                    gld.messages.length === 1)
+                {
+                    findChannel(gld, gld.inquisition_channel).lastMessage.delete();
+
+                    findChannel(gld, gld.inquisition_channel).send(`**${gld.messages[0]}**`);
+
+                    gld.messages.shift();
+                }
+            }
             saveGuild(gld);
         } else if (message.content.startsWith('%target')){
             gld.inquisition_target = message.mentions.members.first(1)[0].id;
+
             saveGuild(gld);
+
             message.channel.send(`Jauns inkvizīcijas mērķis ${message.mentions.members.first(1)[0]}`);
+        } else if (message.content.startsWith('%clear')){
+            gld.messages = [];
+
+            saveGuild(gld);
+
+            message.channel.send(`Jautājumu rinda notīrīta`);
+        } else if (message.content.startsWith('%queue')){
+            let numToShow = (Number.parseInt(message.content.slice(7)) || 10) - 1;
+
+            let finStr = `Jautājumu rinda (${Math.min(numToShow, gld.messages.length)}/${gld.messages.length}):
+`;
+
+            for (let i = 0; i < numToShow; i++){
+                if (!gld.messages[i]){
+                    break;
+                }
+
+                finStr = `${finStr}
+**${gld.messages[i]}**
+`;
+            }
+            message.channel.send(finStr);
+        } else if (message.content.startsWith('%stop')){
+            gld.messages = [];
+            gld.status = 'ready';
+
+            findUser(gld, gld.inquisition_target).roles.remove(gld.inquisition_role, 'Beidzam inkvizīciju');
+            saveGuild(gld);
+
+            message.channel.send(`Inkvizīcija apstādināta`);
         } else if (message.content.startsWith('%start')){
             const user = findUser(gld, gld.inquisition_target);
+
             user.roles.add(gld.inquisition_role, 'Sākam inkvizīciju');
+
             findChannel(gld, gld.inquisition_channel).send(`**Lai inkvizīcija sākas ${user}!** Nākamo jautājumu iegūstam ar "**Next!**", kad vairs inkvizīciju nevari pavilkt "**Enough!**"`);
             findChannel(gld, gld.inquisition_channel).send(`Pirmais jautājums: **${gld.messages[0]}**`);
+
             message.channel.send('Inkvizīcija sākta! Jautājumu skaits: '+gld.messages.length);
-            gld.messages.shift();
+
             if (gld.messages.length>0) {
                 gld.status = 'in-progress';
-            }else{
-                findChannel(gld, gld.inquisition_channel).send(`Diemžēl tas bija arī vienīgais jautājums :/`);
             }
+
+            gld.messages.shift();
             saveGuild(gld);
         }
     }
     if (gld.status === 'in-progress' && message.channel.id === gld.inquisition_channel){
         if (message.content.startsWith('Next!')){
             if (!gld.messages.length>0){
-                findChannel(gld, gld.inquisition_channel).send(`**Inkvizīcija izturēta! ✅**`);
-                gld.status = 'ready';
-                findUser(gld, gld.inquisition_target).roles.remove(gld.inquisition_role, 'Beidzam inkvizīciju');
+                findChannel(gld, gld.inquisition_channel).send(`**Pagaidām jautājumu nav, pagaidi nedaudz, varbūt vēl būs. Jautājumus var iesūtīt šeit: **`);
             }
-            if (gld.messages.length==1){
-                findChannel(gld, gld.inquisition_channel).send(`Pēdējais jautājums: **${gld.messages[0]}**`);
-            } else if (gld.messages.length>0) {
+            
+            if (gld.messages.length>0) {
                 findChannel(gld, gld.inquisition_channel).send(`**${gld.messages[0]}**`);
             }
             gld.messages.shift();
+
             saveGuild(gld);
         } else if (message.content.startsWith('Enough!')){
             findChannel(gld, gld.inquisition_channel).send(`**Inkvizīcija neizturēta! ❎**`);
+
             gld.messages = [];
             gld.status = 'ready';
+
             findUser(gld, gld.inquisition_target).roles.remove(gld.inquisition_role, 'Beidzam inkvizīciju');
             saveGuild(gld);
         }
+    }
+}
+
+client.on('ready', () => {
+    console.log('I am ready!');
+    if (client.guilds){
+        client.guilds.cache.each((gld) => loadGuild(gld));
+    }
+});
+
+client.on('guildCreate', (gld) => {
+    console.log('added to a new guild');
+
+    guilds[gld.id] = {guild: gld, status: 'no-channel', messages: []};
+
+    jsonfile.writeFile(gld.id+'.json', {status: 'setup'});
+});
+
+client.on('message', (message) => {
+    try {
+        onMessageHandler(message)
+    } catch (err) {
+        console.log(err);
+        message.channel.send('Notika kāda kļūda, pamēģini ievadīt komandu vēlreiz');
     }
 })
